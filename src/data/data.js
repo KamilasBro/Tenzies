1f488b6640ff7179bc8ffdc92ad3c1946e85f514
siema <mrbrogopro@gmail.com>
2023-02-18 02:09:05 +0100

    Configure React app for deployment to GitHub Pages

--- src/data/data.js
+++ src/data/data.js
@@ -1,53 +1,55 @@
 export default function data()
-return([
-    {
-        id: 1,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 2,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 3,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 4,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 5,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 6,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 7,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 8,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 9,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    },
-    {
-        id: 10,
-        freeze: false,
-        digit: Math.floor(Math.random() * 6) + 1
-    }
-])
+{
+    return([
+        {
+            id: 1,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 2,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 3,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 4,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 5,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 6,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 7,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 8,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 9,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        },
+        {
+            id: 10,
+            freeze: false,
+            digit: Math.floor(Math.random() * 6) + 1
+        }
+    ])
+}
